f45ab047a60e257a8bc9beb06004ecf8514480f5
Ивлев Вадим <ivlev@rg.ru>
2020-08-19 15:15:02 +0300

    .

--- db.py
+++ db.py
@@ -1,8 +1,37 @@
 # Функции роваты с базой данных
 
 
+import sqlite3
+
+db_filename = 'rg.db'
+
+# Запросы на порождение таблиц
+sql_create_tables = """
+CREATE TABLE IF NOT EXISTS rubrics (
+    id TEXT PRIMARY KEY,
+    parent_id TEXT,
+    title TEXT,
+    uri TEXT
+);
+"""
+
+
+def create_database():
+    conn = sqlite3.connect(db_filename)
+    c = conn.cursor()
+    c.execute(sql_create_tables)
+    conn.commit()
+    conn.close()
+
+
 
 
 def save_rubric(id = 0, parent_id = None, title="", uri=""):
     "Сохраняет рубрику в базу данных"
     print(f'id:{id} parent_id:{parent_id} title:{title} uri:{uri}')
+
+    conn = sqlite3.connect(db_filename)
+    c = conn.cursor()
+    c.execute("INSERT INTO rubrics VALUES (?,?,?,?)", (id,parent_id,title,uri) )
+    conn.commit()
+    conn.close()
--- main.py
+++ main.py
@@ -2,5 +2,8 @@
 # Считывает данные из API RGRU и сохраняет их в базе данных
 
 import api
+import db
 
+
+db.create_database()
 api.get_rubrics()
