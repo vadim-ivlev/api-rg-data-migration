f035d477f30e75cf333afa663ae75ffb8ec84922
vadim-ivlev <vadim.ivlev@gmail.com>
2020-10-25 01:09:08 +0300

    .

--- sh/build-executable.sh
+++ sh/build-executable.sh
@@ -15,4 +15,4 @@ export GO111MODULE=on
 # docker run --rm -it -v "$PWD":/usr/src/myapp -w /usr/src/myapp -e CGO_ENABLED=1 -e GOOS=linux golang:1.15 go build -a -ldflags '-linkmode external -extldflags "-static"'
 
 # Компиляция под текцщую операционку
-env CGO_ENABLED=0 go build -a -ldflags '-extldflags "-static"' -o python/save_articles .
+env CGO_ENABLED=0 go build -a -ldflags '-extldflags "-static"' -o python/save_articles .ƒ
