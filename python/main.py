ced43bf301512b5787fff6634a7f9d829df345fd
vadim-ivlev <vadim.ivlev@gmail.com>
2021-04-09 17:38:41 +0300

    fix: messaging

--- python/main.py
+++ python/main.py
@@ -11,6 +11,13 @@ sleep_time = 60 * 5
 # счетчик попыток
 counter =0
 
+def message_and_sleep():
+    dt = datetime.datetime.now(pytz.timezone('Europe/Moscow')).strftime('%Y-%m-%d %H:%M:%S')
+    print(f'Ждем {sleep_time/60:.0f} минут после обновления # {counter} {dt}...')
+    print('----------------------------------\n')
+    time.sleep(sleep_time)    
+
+
 # Повторяем одни и те же действия
 while True:
     counter += 1
@@ -18,20 +25,17 @@ while True:
 
     # Обновляем таблицу рубрик
     if not save_rubrics.main():
-        print(f"save_rubrics.main() вернула False. Ждем {sleep_time/60} мин ...")
-        time.sleep(sleep_time)
+        print("save_rubrics.main() вернула False.")
+        message_and_sleep()
         continue
     
     # Обновляем таблицу связи рубрики-объекты
     if not save_rubrics_objects.main():
-        print(f"save_rubrics_objects.main() вернула False. Ждем {sleep_time/60} мин ...")
-        time.sleep(sleep_time)
+        print(f"save_rubrics_objects.main() вернула False.")
+        message_and_sleep()
         continue
     
     # Обновляем таблицу статей с помощью вызова go программы.
     path = sys.path[0]
     os.system(path+'/save_articles')
-    dt = datetime.datetime.now(pytz.timezone('Europe/Moscow')).strftime('%Y-%m-%d %H:%M:%S')
-    print(f'Ждем {sleep_time/60:.0f} минут после обновления # {counter} {dt}...')
-    print('----------------------------------\n')
-    time.sleep(sleep_time)
+    message_and_sleep()
